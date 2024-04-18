c2e3dbcbb363c240db1af10f3d51b1bbb66cfdce
fyyshih <felithecity47@gmail.com>
2024-04-17 23:48:41 -0700

    create

--- Create.js
+++ Create.js
@@ -1,9 +1,9 @@
 function createDrawing() {
     // Specify the color for clearing <canvas>
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+   // gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
     // Clear <canvas>
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    //gl.clear(gl.COLOR_BUFFER_BIT);
 
     // create drawing using triangles
     let test = new Triangle();
